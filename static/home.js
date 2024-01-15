fecc7ce8b16a2f5e7192634088128c6f2e6b305e
Noah Mekonnen <noahmekonnen0@gmail.com>
2024-01-15 02:53:37 -0500

    finishing touches

--- static/home.js
+++ static/home.js
@@ -5,31 +5,35 @@ const body = $('body')
 
 async function getList(){
     const resp = await axios.get('/api/cupcakes')
-    return resp.data
+    listOfCupcakes = resp.data
+    for (cupcake of listOfCupcakes.cupcakes){
+        ul.append(`<li>
+        flavor: ${cupcake.flavor}
+        size: ${cupcake.size}
+        rating: ${cupcake.rating}
+        <img src=${cupcake.image}>
+        </li>`)
+    }
 }
-
-const listOfCupcakes = getList()
-console.log(listOfCupcakes)
-for (cupcake of listOfCupcakes){
-    body.append(`<li>
-    flavor: ${cupcake.flavor}
-    size: ${cupcake.size}
-    rating: ${cupcake.rating}
-    <img src=${cupcake.image}>
-    </li>`)
-}
-
-form.on('submit', postRequest)
 
 async function postRequest(e){
     e.preventDefault()
     const flavor = $('#flavor')[0].value
     const size = $('#size')[0].value
     const rating = $('#rating')[0].value
+    console.log(rating)
     const image = $('#image')[0].value
     axios.post("/api/cupcakes",
     {"flavor":flavor,
     "size":size,
     "rating":parseFloat(rating),
     "image":image})
+    ul.append(`<li>
+    flavor: ${flavor}
+    size: ${size}
+    rating: ${rating}
+    <img src=${image}></li>`)
 }
+
+getList()
+form.on('submit', postRequest)
